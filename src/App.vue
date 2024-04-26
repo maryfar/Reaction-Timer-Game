146f79dc9f8b9d0fdb5bf0a585fc0f7d8e0a0f5e
maryfar <maryam.farhadi.67@gmail.com>
2024-04-26 23:11:25 +0330

    vue project: reaction timer game

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+
+
+<template>
+ <h1>OpenCode Reaction Timer </h1>
+ <button @click="start" :disabled="isPlaying">Play</button>
+ <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+ <Results v-if="showResult" :score="score" />
+</template>
+
+<script >
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+export default {
+  name: 'App',
+  components:{Block,Results},
+  data() {
+    return {
+      delay:null,
+      isPlaying:false,
+      showResult:false,
+      score: null
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random()* 5000
+      this.isPlaying = true;
+      this.showResult = false;
+      console.log(this.delay)
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResult = true
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+#app {
+text-align: center;
+color: #444;
+margin-top: 60px;
+}
+
+button {
+  background: #0faf87;
+  color:white;
+  border:none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin:10px;
+}
+
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+
+}
+
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+    <p>Reaction Time: {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+
+</template>
+
+<script>
+export default {
+    props:['score'],
+    data(){
+        return {
+            rank:null,
+        }
+    },
+    mounted(){
+        if (this.score<400){
+            this.rank = "You are an expert"
+        }
+        else if (this.score<800){
+            this.rank = "You are an intermediate"
+        }
+        else{
+            this.rank = "You are a biginner "
+        }
+    }
+}
+
+
+</script>
+
+<style>
+.rank {
+    font-size: 1.4em;
+    color:#419e87;
+    font-weight: 500;
+}
+
+</style>
